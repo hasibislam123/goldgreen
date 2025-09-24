0366aea6fcfbd587fb5c51d410e7946f5f97c7d2
Hasib Islam <hasib64dj@gmail.com>
2025-09-24 10:28:37 +0600

    added the new code

---
+++ src/componet/Banner/Banner.jsx
@@ -0,0 +1,89 @@
+import React from 'react';
+
+const Banner = () => {
+    return (
+        <div>
+
+    <div>
+        <h1>Keep your body fresh with a good perfume</h1>
+        <p>Lorem ipsum dolor sit amet consectetur. Scelerisque amet cursus eget amet sit ut. In imperdiet suspendisse adipiscing eu purus. Eget nisl cursus quis nibh. Sed sit amet facilisi viverra.</p>
+    </div>
+
+    <div class="carousel w-full">
+  <div id="slide1" class="carousel-item relative w-full h-200">
+    <img src="https://i.ibb.co/bjGWKmxY/tareq-hasan-z-Zca-CSE2-Bl8-unsplash.jpg" class="w-full h-full object-cover" />
+    <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+      <a href="#slide10" class="btn btn-circle">❮</a>
+      <a href="#slide2" class="btn btn-circle">❯</a>
+    </div>
+  </div> 
+  <div id="slide2" class="carousel-item relative w-full h-full">
+    <img src="https://i.ibb.co/VpTKC6DW/eve-maier-b-G7-JSKq5-W5-E-unsplash.jpg" class="w-full h-full object-cover" />
+    <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+      <a href="#slide1" class="btn btn-circle">❮</a>
+      <a href="#slide3" class="btn btn-circle">❯</a>
+    </div>
+  </div> 
+  <div id="slide3" class="carousel-item relative w-full h-full">
+    <img src="https://i.ibb.co/DDp4XybQ/maxim-lozyanko-q-Fsxwpo-DIB4-unsplash.jpg" class="w-full h-full object-cover" />
+    <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+      <a href="#slide2" class="btn btn-circle">❮</a>
+      <a href="#slide4" class="btn btn-circle">❯</a>
+    </div>
+  </div> 
+  <div id="slide4" class="carousel-item relative w-full h-full">
+    <img src="https://i.ibb.co/JWWNWxCZ/rosy-h-nguyen-5-Hrl-EGTsa-B4-unsplash.jpg" class="w-full h-full object-cover" />
+    <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+      <a href="#slide3" class="btn btn-circle">❮</a>
+      <a href="#slide5" class="btn btn-circle">❯</a>
+    </div>
+  </div>
+  <div id="slide5" class="carousel-item relative w-full h-full">
+    <img src="https://i.ibb.co/Jw32FZ29/tron-le-E5zsok-LJk-VQ-unsplash.jpg" class="w-full h-full object-cover" />
+    <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+      <a href="#slide4" class="btn btn-circle">❮</a>
+      <a href="#slide6" class="btn btn-circle">❯</a>
+    </div>
+  </div>
+  <div id="slide6" class="carousel-item relative w-full h-full">
+    <img src="https://i.ibb.co/TxQx0skR/olena-bohovyk-w-V0-Or-CO90e-I-unsplash.jpg" class="w-full h-full object-cover" />
+    <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+      <a href="#slide5" class="btn btn-circle">❮</a>
+      <a href="#slide7" class="btn btn-circle">❯</a>
+    </div>
+  </div>
+  <div id="slide7" class="carousel-item relative w-full h-full">
+    <img src="https://i.ibb.co.com/Ndz5P0M2/pavlo-talpa-56-YB6-Ycu1-C4-unsplash.jpg" class="w-full h-full object-cover" />
+    <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+      <a href="#slide6" class="btn btn-circle">❮</a>
+      <a href="#slide8" class="btn btn-circle">❯</a>
+    </div>
+  </div>
+  <div id="slide8" class="carousel-item relative w-full h-full">
+    <img src="https://i.ibb.co.com/YgtzTFS/pavlo-talpa-Mf-Go-Z-Qo-JFc-unsplash.jpg" class="w-full h-full object-cover" />
+    <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+      <a href="#slide7" class="btn btn-circle">❮</a>
+      <a href="#slide9" class="btn btn-circle">❯</a>
+    </div>
+  </div>
+  <div id="slide9" class="carousel-item relative w-full h-full">
+    <img src="https://i.ibb.co.com/5WKhh94H/edoardo-cuoghi-ufuchd-Zawng-unsplash.jpg" class="w-full h-full object-cover" />
+    <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+      <a href="#slide8" class="btn btn-circle">❮</a>
+      <a href="#slide10" class="btn btn-circle">❯</a>
+    </div>
+  </div>
+  <div id="slide10" class="carousel-item relative w-full h-full">
+    <img src="https://i.ibb.co.com/LhJj5BcW/gift-habeshaw-9-I-h-AW8-JUUg-unsplash.jpg" class="w-full h-full object-cover" />
+    <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+      <a href="#slide9" class="btn btn-circle">❮</a>
+      <a href="#slide1" class="btn btn-circle">❯</a>
+    </div>
+  </div>
+    </div>
+
+</div>
+    );
+};
+
+export default Banner;
